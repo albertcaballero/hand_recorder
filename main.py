557f3d2078be54e20a7ac6f63ad10f2e65455517
Albert Caballero <albertcaballerocoll@gmail.com>
2024-03-25 00:33:28 +0100

    tracking works, stopped at recording stage

--- main.py
+++ main.py
@@ -8,14 +8,35 @@ error_margin = 0.1
 
 def check_pose(pose):
 	global error_margin
-	pass
+	return
+
+def normalize_pose(pose):
+	base = pose.landmark[0] #apparently this is not a deep copy and it's the same pointer, to do deep copy copy values manually xd
+	print ("________base________\n", base)
+	i = 0
+	#get distance from land[0] to land[5] to determine the (distance from camera/size of hand) and make it relative
+	while i < 21:
+		print("before:", pose.landmark[i])
+		pose.landmark[i].x -= base.x
+		pose.landmark[i].y -= base.y
+		pose.landmark[i].z -= base.z
+		print("after:", pose.landmark[i])
+		print ("base still:", base, "i is", i)
+		i += 1
+	print ("====================\n")
 
 def record_pose(frame, results):
 	countdown = 150
 	if (countdown > 0):
 		cv.putText(frame, text="3", org=(100,100), fontFace=cv.FONT_HERSHEY_TRIPLEX, fontScale=2, color=(100,100,100), thickness=3)
 		countdown -= 1
-	new_pose = results.multi_hand_landmarks
+	if (results.multi_hand_landmarks):
+		new_pose = results.multi_hand_landmarks[0]
+		normalize_pose(new_pose)
+		return new_pose
+	else:
+		print("no pose detected")
+		return False
 
 def capturing(mp_drawing, mp_hands):
 	capture = cv.VideoCapture(0)
@@ -33,11 +54,12 @@ def capturing(mp_drawing, mp_hands):
 			results = hands.process(frame)
 			frame.flags.writeable = True
 			# image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
-			if (results.hand_landmarks):
-				mp_drawing.draw_landmarks(frame, results.hand_landmarks, mp_hands.HAND_CONNECTIONS)
+			if (results.multi_hand_landmarks):
+				for hand_landmarks in results.multi_hand_landmarks:
+					mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 				check_pose(results)
-			cv.imshow('Squat King', frame)
-			if cv.waitKey(1) == ord('r'):
+			cv.imshow('Pose recorder', frame)
+			if kb.is_pressed('r+e+c'):
 				record_pose(frame, results)
 			if cv.waitKey(1) == ord('q'):
 				break
@@ -48,9 +70,3 @@ mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 # saved_poses = load_poses("poses.pose")
 capturing(mp_drawing, mp_hands)
-
-# 1. json :)
-# 2. please please please i know it's python but optimize it plsssssssss
-# 3. points positions are relative not absolute (relative to nose maybe??)
-
-#points: 1 nose, 2 eyes, 2 hands, 2 wrists, 2 elbows, 2 shoulders, 2 hips, 2 knees, 2 ankles = 17 points max
